f33f49213c497f565cc0ce7eaa0805c5eff85e6b
kitmi <juadolfob@gmail.com>
2022-05-16 11:22:50 -0600

    eureka

--- basic/src/main/java/com/example/basic/BasicApplication.java
+++ basic/src/main/java/com/example/basic/BasicApplication.java
@@ -2,7 +2,9 @@ package com.example.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
+@EnableEurekaClient
 public class BasicApplication {
 
 	public static void main(String[] args) {
