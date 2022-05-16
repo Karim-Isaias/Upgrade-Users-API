e9b54e2e0959dd7375d324a015e50909f44e2f6c
kitmi <juadolfob@gmail.com>
2022-05-16 15:10:47 -0600

    Circuit Breaker

--- basic/src/main/java/com/example/basic/Controllers/BaseControllerImpl.java
+++ basic/src/main/java/com/example/basic/Controllers/BaseControllerImpl.java
@@ -2,10 +2,15 @@ package com.example.basic.Controllers;
 
 import com.example.basic.Entities.Base;
 import com.example.basic.Services.BaseServiceImpl;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public abstract class BaseControllerImpl<E extends Base, S extends BaseServiceImpl<E, Long>> implements BaseController<E, Long> {
@@ -13,28 +18,54 @@ public abstract class BaseControllerImpl<E extends Base, S extends BaseServiceIm
     @Autowired
     protected S servicio;
 
+    @CircuitBreaker(name = "getAllCB", fallbackMethod = "fallbackgetAllCB")
     @GetMapping("")
     public CompletableFuture<ResponseEntity> getAll() {
         return servicio.Getall().thenApply(ResponseEntity::ok);
     }
 
+    @CircuitBreaker(name = "getOneCB", fallbackMethod = "fallbackgetOneCB")
     @GetMapping("/{id}")
     public CompletableFuture<ResponseEntity> getOneAsync(@PathVariable Long id) throws Exception {
         return servicio.findByIdAsync(id).thenApply(ResponseEntity::ok);
     }
+    
+    @CircuitBreaker(name = "saveCB", fallbackMethod = "fallbacksaveCB")
     @PostMapping("")
     public CompletableFuture<ResponseEntity> save(@RequestBody E entity) throws Exception {
         return servicio.save(entity).thenApply(ResponseEntity::ok);
     }
+    
+    @CircuitBreaker(name = "updateCB", fallbackMethod = "fallbackupdateCB")
     @PutMapping("/{id}")
     public CompletableFuture<ResponseEntity> update(@PathVariable Long id, @RequestBody E entity) throws Exception {
         return servicio.update(id, entity).thenApply(ResponseEntity::ok);
     }
 
+    @CircuitBreaker(name = "deleteCB", fallbackMethod = "fallbackdeleteCB")
     @DeleteMapping("/{id}")
     public CompletableFuture<ResponseEntity> delete(@PathVariable Long id) throws Exception {
             return servicio.delete(id).thenApply(ResponseEntity::ok);
 
     }
-
+    
+    private ResponseEntity fallbackgetAllCB(RuntimeException e) {
+        return new ResponseEntity("getAll no disponible", HttpStatus.OK);
+    }
+    
+    private ResponseEntity fallbackgetOneCB(@PathVariable Long id, RuntimeException e) {
+        return new ResponseEntity("getOne no disponible", HttpStatus.OK);
+    }
+    
+    private ResponseEntity fallbacksaveCB(@RequestBody E entity, RuntimeException e) {
+        return new ResponseEntity("save no disponible", HttpStatus.OK);
+    }
+    
+    private ResponseEntity fallbackupdateCB(@PathVariable Long id, RuntimeException e) {
+        return new ResponseEntity("update no disponible", HttpStatus.OK);
+    }
+    
+    private ResponseEntity fallbackdeleteCB(@PathVariable Long id, RuntimeException e) {
+        return new ResponseEntity("delete no disponible", HttpStatus.OK);
+    }
 }
