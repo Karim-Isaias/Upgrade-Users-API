2e69e5b71eae76397c5f95ea9ed36abf8b8b4e41
kitmi <juadolfob@gmail.com>
2022-06-15 20:34:01 -0600

    jwt

--- gateway-service/src/main/java/com/example/gatewayservice/security/SecurityConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/security/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.example.gatewayservice.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
 
 @Configuration
 @EnableWebFluxSecurity
@@ -13,10 +24,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
-        http.authorizeExchange().anyExchange().authenticated()
+        http.authorizeExchange().anyExchange().permitAll()
                 .and()
-                .oauth2Login(Customizer.withDefaults());
-        http.csrf().disable();
+                .oauth2Login()
+                .and()
+                .oauth2Client();
+
         return http.build();
     }
 }
